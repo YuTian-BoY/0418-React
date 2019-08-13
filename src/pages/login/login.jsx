c3d24670d33424a8b90fe97feb24ab78e1a0d94a
yutian <woshiyutian@outlook.com>
2019-08-13 09:36:55 +0800

    new login

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -8,10 +8,15 @@ import {
 
 import './login.less';
 import logo from './images/logo.png'
+
+const Item = Form.Item
 /* 
 登录路由组件
 */
 export default class Login extends Component {
+    handleSubmit = e => {
+        e.preventDefault()
+    }
     render() {
         return (
             <div className="login">
@@ -21,7 +26,26 @@ export default class Login extends Component {
                 </div>
                 <div className="login-content">
                     <h1>用户登录</h1>
-                    <div>From</div>
+                    <Form onSubmit={this.handleSubmit} className="login-form">
+                        <Item>
+                            <Input
+                                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                placeholder="请输入用户名"
+                            />
+                        </Item>
+                        <Item>
+                            <Input
+                                    prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                    type="password"
+                                    placeholder="请输入密码"
+                                />
+                        </Item>
+                        <Item>
+                            <Button type="primary" htmlType="submit" className="login-form-button">
+                                登录
+                            </Button>
+                        </Item>
+                    </Form>
                 </div>
             </div>
         )
