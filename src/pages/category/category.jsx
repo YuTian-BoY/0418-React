9ff5a3c51e6f17ca3d42e27199bfa3871fffe329
yutian <woshiyutian@outlook.com>
2019-08-20 08:33:16 +0800

    new 2019/8/20 react

--- src/pages/category/category.jsx
+++ src/pages/category/category.jsx
@@ -1,13 +1,65 @@
 import React, { Component } from 'react'
+import { Card, Button, Icon, Table } from 'antd'
+import LinkButton from '../../components/link-button';
+import { reqCategorys } from '../../api'
+
+
+
+
 /**
  * 分类管理
  */
 export default class Category extends Component {
+  stare = {
+    category: []
+  }
+
+  /* 异步获取所有分类列表显示 */
+  getCategory = async () => {
+    const result = await reqCategorys()
+    if (result.status === 0) {
+      const categorys = result.data
+      this.setState({
+        categorys
+      })
+    }
+  }
+
+
+  componentDidMount() {
+    this.getCategorys()
+  }
+
+
   render() {
+    const columns = [
+      {
+        title: '分类名称',
+        dataIndex: 'name',
+      },
+      {
+        width: 300,
+        title: '操作',
+        render: () => <LinkButton>修改分类</LinkButton>
+      },
+    ];
+    //取出状态数据 
+    const { categorys } = this.state
+    const extra = (
+      <Button type='primary'>
+        <Icon type='plus' />
+        添加
+      </Button>
+    )
+
     return (
-      <div>
-        Category
-      </div>
+      <Card extra={extra}>
+        <Table
+          bordered
+          dataSource={categorys}
+          columns={columns}
+        />
+      </Card>
     )
   }
 }
